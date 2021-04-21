a5fb74e5a9e1d571a948cef72335860dc6398ad2
Desktopy12 <Keeley.lp@gmail.com>
2021-04-21 15:58:14 +0100

    updated with additional styling and card component with image references

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css'
+import ImageCard from './ImageCard.js';
 import React from 'react';
 
 const ImageList=(props)=>{
 const images= props.images.map((image)=>{
 
-    return <img src={image.urls.regular}></img>
+    return <ImageCard  key={image.id} image={image} ></ImageCard>
 })
-return <div>{images}</div>
+return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
